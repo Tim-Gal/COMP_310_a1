3ec76fb96a3fb79933ccc0a1545eb4b97f46ddd3
Tim <timoxa.gal@gmail.com>
2023-10-14 15:51:48 -0400

    extract builtins from fork()

--- a1.c
+++ a1.c
@@ -2,17 +2,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-
-struct job
-{
-    pid_t pid;
-    char* name;
-};
-
-struct job jobs[10];
 
 
 int getcmd(char *prompt, char *args[], int *background)
@@ -21,15 +10,15 @@ int getcmd(char *prompt, char *args[], int *background)
     char *token, *loc;
     char *line = NULL;
     size_t linecap = 0;
-
     printf("%s", prompt);
     length = getline(&line, &linecap, stdin);
+
     if (length <= 0)
     {
+        free(line);
         exit(-1);
     }
 
-    // Check if background is specified..
     if ((loc = index(line, '&')) != NULL)
     {
         *background = 1;
@@ -48,6 +37,9 @@ int getcmd(char *prompt, char *args[], int *background)
         if (strlen(token) > 0)
             args[i++] = token;
     }
+
+    args[i] = NULL;
+
     free(line);
     return i;
 }
@@ -55,72 +47,45 @@ int getcmd(char *prompt, char *args[], int *background)
 
 int main(void)
 {
-    char *buffer;
-    size_t size = 1024;
-    buffer = (char *)malloc((size_t)size);
-
+    char *args[20];
     int bg;
-    while(1)
-    {
-        char *args[20] = { NULL };
+    while(1) {
+
+        for (int i = 0; i < 20; i++)
+        {
+            args[i] = NULL;
+        }
+
         bg = 0;
         int cnt = getcmd("\n>> ", args, &bg);
-        /* the steps can be..:
-        (1) fork a child process using fork()
-        (2) the child process will invoke execvp()
-        (3) if background is not specified, the parent will wait,
-        otherwise parent starts the next command... */
 
-        /*
-        for (int i = 0; args[i] != NULL; i++) {
-            printf("%d", i);
-            printf("Argument %d: %s\n", i, args[i]);
-        }        
-        */
-
-        pid_t child_pid;
-        child_pid = fork();
-
-        if (child_pid == -1)
+        if (args[0] == NULL)
         {
-            perror("fork");
-            exit(EXIT_FAILURE);
+            printf("No command entered.\n");
+            continue;
         }
-        else if (child_pid == 0)
+
+        if (strcmp(args[0], "echo") == 0)
         {
-            // This is the child process
+            for (int i = 1; args[i] != NULL; i++)
+                printf("%s ", args[i]);
+            printf("\n");
         }
-        else
+        else if (strcmp(args[0], "cd") == 0)
         {
-            // This is the parent process
-            if (strcmp(args[0], "echo") == 0)
-            {
-                for (int i = 1; args[i] != NULL; i++)
-                    printf("%s ", args[i]);
-                printf("\n");
-                fflush(stdout);
-            }
-            else if (strcmp(args[0], "cd") == 0)
-            {
-                chdir(args[1]);
-            }
-            else if (strcmp(args[0], "pwd") == 0)
-            {
-                getcwd(buffer, size);
-                printf("%s", buffer);
-            }
-            else if (strcmp(args[0], "exit") == 0)
-            {
-                exit(0);
-            }
-            else if (strcmp(args[0], "jobs") == 0)
-            {                
-                for (int i = 1; i<10; i++)
-                {
-                    printf("job %d is %s\n", i, jobs[i].name);
-                }
-            }
-            wait(NULL);
+            chdir(args[1]);
+        }
+        else if (strcmp(args[0], "pwd") == 0)
+        {
+            char *buffer;
+            size_t size = 1024;
+            buffer = (char *)malloc(size);
+            getcwd(buffer, size);
+            printf("%s\n", buffer);
+        }
+        else if (strcmp(args[0], "exit") == 0)
+        {
+            exit(0);
         }
     }
 }
